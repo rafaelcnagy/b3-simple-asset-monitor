99911977606815b7dda284af5b834127ce281adb
Rafael Correa Nagy <rafaelcnagy@hotmail.com>
2022-02-24 20:34:19 -0300

    Schedules only if runserver

--- monitoring/apps.py
+++ monitoring/apps.py
@@ -1,9 +1,14 @@
 from django.apps import AppConfig
+import sys 
 
 class MonitoringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'monitoring'
 
     def ready(self):
-        from . import scheduler
-        scheduler.start()
+        is_manage_py = any(arg.casefold().endswith("manage.py") for arg in sys.argv)
+        is_runserver = any(arg.casefold() == "runserver" for arg in sys.argv)
+
+        if (is_manage_py and is_runserver) or (not is_manage_py):
+            from . import scheduler
+            scheduler.start()
